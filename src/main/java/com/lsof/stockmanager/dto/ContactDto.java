46f183e64fc1ef478a38d1d1b663c37bd75897fc
Sofia <lsofmascastanio@gmail.com>
2024-04-10 14:14:03 +0200

    ContactDto add

---
+++ src/main/java/com/lsof/stockmanager/dto/ContactDto.java
@@ -0,0 +1,50 @@
+package com.lsof.stockmanager.dto;
+
+import com.lsof.stockmanager.model.Contact;
+import com.lsof.stockmanager.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class ContactDto {
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String iban;
+
+    private Integer userId;
+
+    public static ContactDto fromEntity(Contact contact){
+        return ContactDto.builder()
+                .firstname(contact.getFirstname())
+                .lastname(contact.getLastname())
+                .email(contact.getEmail())
+                .iban(contact.getIban())
+                .userId(contact.getUser().getId())
+                .build();
+    }
+
+    public static Contact toEntity(ContactDto contactDto){
+        return Contact.builder()
+                .firstname(contactDto.getFirstname())
+                .lastname(contactDto.getLastname())
+                .email(contactDto.getEmail())
+                .iban(contactDto.getIban())
+                .user(
+                        User.builder()
+                                .id(contactDto.getUserId())
+                                .build()
+                )
+                .build();
+    }
+
+}
