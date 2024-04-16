4865f42ad0c84a518c3cc2b5f73d67696c076066
Sofia <lsofmascastanio@gmail.com>
2024-04-16 10:38:18 +0200

    findAllByUserId method added to TransactionServiceDefault

--- src/main/java/com/lsof/stockmanager/service/TransactionServiceContract.java
+++ src/main/java/com/lsof/stockmanager/service/TransactionServiceContract.java
@@ -1,6 +1,10 @@
 package com.lsof.stockmanager.service;
 
 import com.lsof.stockmanager.dto.TransactionDto;
+import com.lsof.stockmanager.model.Transaction;
+
+import java.util.List;
 
 public interface TransactionServiceContract extends GenericDbService<TransactionDto>{
+    List<TransactionDto> findAllByUserId(Integer userId);
 }
